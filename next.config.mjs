6eb345bd6414ffe5ee18d62061417b28d211fac4
MOHAMED <mohamed1239455@gmail.com>
2024-01-30 20:16:05 +0200

    github

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default {
-  nextConfig,
-  env: {
-    GITHUB_ID: process.env.GITHUB_ID,
-    GITHUB_SECRET: process.GITHUB_SECRET,
-  },
-};
+export default nextConfig;
